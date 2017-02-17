e5e46e6d377abc5f5b9e01d0970a1a0e4c9b5084
Boris Valo <VALOB01@DIXONSRETAIL.COM>
2017-02-17 09:58:02 +0100

    [Czechitas] First task, created header.php file

--- web-final/index.php
+++ web-final/index.php
@@ -34,19 +34,7 @@ $workExperiences = loadData(WORK_EXPERIENCES_FILE);
 
 <?php include_once 'menu.php' ?>
 
-<header class="intro-header" style="background-image: url('img/home-bg.jpg')">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
-                <div class="site-heading">
-                    <h1><?=getWebTitle()?></h1>
-                    <hr class="small">
-                    <span class="subheading"><?php echo $owner; ?></span>
-                </div>
-            </div>
-        </div>
-    </div>
-</header>
+<?php include_once 'header.php' ?>
 
 <div class="container">
     <div class="row">
